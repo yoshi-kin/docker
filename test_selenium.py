c73769c3a179619cf645db58bca3907b418c3415
yoshi-kin <55827044+yoshi-kin@users.noreply.github.com>
2021-11-05 07:49:46 +0900

    Update test_selenium.py

--- test_selenium.py
+++ test_selenium.py
@@ -7,6 +7,7 @@ options = Options()
 # ヘッドレスモード（Linux上で動かすとき必ずこのモードにしておく）
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(options=options)
 driver.get('https://example.com')
 print(driver.current_url)
