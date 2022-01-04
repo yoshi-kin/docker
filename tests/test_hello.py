c84c7756bab16fdf8362d130bb7b2205e7b089db
yoshikifujiwara <fujiwara@offgrid.co.jp>
2022-01-04 11:37:57 +0900

    update

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -4,6 +4,7 @@ from psycopg2.extras import DictCursor
 
 def pg_conn():
     setting = {
+        'host':"/tmp/",
         'port': '5432',
         'dbname': 'in_system',
         'user': 'postgres',
