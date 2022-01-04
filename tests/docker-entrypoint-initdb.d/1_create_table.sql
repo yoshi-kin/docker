8bdba83330620431f4682b35365910c688ace232
yoshikifujiwara <fujiwara@offgrid.co.jp>
2022-01-04 11:48:34 +0900

    update

--- tests/docker-entrypoint-initdb.d/1_create_table.sql
+++ tests/docker-entrypoint-initdb.d/1_create_table.sql
@@ -1,4 +1,10 @@
-create table if not exists users (
+create database in_system;
+
+
+\c in_system
+
+
+create table users (
   id serial not null
   , name character varying not null
   , constraint users_PKC primary key (id)
