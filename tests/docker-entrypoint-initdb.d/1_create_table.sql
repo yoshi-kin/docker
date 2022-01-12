9072b0d948324a3e3073c4e8e4445f47c9f0f21a
yoshikifujiwara <fujiwara@offgrid.co.jp>
2022-01-12 13:54:08 +0900

    update

--- tests/docker-entrypoint-initdb.d/1_create_table.sql
+++ tests/docker-entrypoint-initdb.d/1_create_table.sql
@@ -444,7 +444,7 @@ create table company (
 
 
 -- 初期データ
-insert into users(user_id, user_name, mail, password, init_flag, lock_flag, status) values('82', 'paiza', 'r.suzuki@offgrid.co.jp', 'pbkdf2:sha256:260000$LJycIHSaEItvnwbz$73625cb4c05b95a88137a4cda74d5c94bec49ba56f68191bc0c07842b7b986e3', '1', '0', '0');
+insert into users(user_id, name, mail, password, init_flag, lock_flag, status) values('82', 'paiza', 'r.suzuki@offgrid.co.jp', 'pbkdf2:sha256:260000$LJycIHSaEItvnwbz$73625cb4c05b95a88137a4cda74d5c94bec49ba56f68191bc0c07842b7b986e3', '1', '0', '0');
 -- insert into user_grant(user_id, grant_id, grant_code) values('82', '101', '2');
 -- insert into user_grant(user_id, grant_id, grant_code) values('82', '102', '2');
 -- insert into user_grant(user_id, grant_id, grant_code) values('82', '103', '2');
